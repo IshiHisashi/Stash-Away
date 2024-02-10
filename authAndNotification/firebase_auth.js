e32a837e493c5c6e5833384cb80ac52601b346ef
Sacha <150120887+c-est-sa@users.noreply.github.com>
2024-02-10 15:57:14 -0800

    auth and notification test pages added

---
+++ authAndNotification/firebase_auth.js
@@ -0,0 +1,40 @@
+"use strict";
+
+// Import the functions you need from the SDKs you need
+// from "firebase/app"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// from "firebase/firestore"
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+
+// Your web app's Firebase configuration
+// for project
+const firebaseConfig = {
+  apiKey: "AIzaSyA0Px8PkiCzyTrDcFCWh-mbER-YcWd9d-E",
+  authDomain: "fir-jan24.firebaseapp.com",
+  projectId: "fir-jan24",
+  storageBucket: "fir-jan24.appspot.com",
+  messagingSenderId: "831417179844",
+  appId: "1:831417179844:web:c3eb03b7fc9c6ef7b03391",
+  measurementId: "G-DSYKEF99M1",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+  auth,
+};
