f8efc7e4f819b8be5550b196bff9460dc219775b
Akiodesukedo <abtai0227aki@gmail.com>
2024-02-16 12:11:28 -0800

    Removed hard-coded part
    * Revised cityArray made with data from DB
    * Made the list into a form
    * Added a submit btn to set user's choice in "selectedCity" object.

--- home-page-map/distance-sort.js
+++ home-page-map/distance-sort.js
@@ -8,9 +8,10 @@ console.log(geo.storageLocationArr);
 console.log(geo.userAddress);
 
 class City {
-  constructor(name, geoLoc) {
+  constructor(name, geoLoc, address) {
     this.name = name;
     this.geoLoc = geoLoc;
+    this.address = address;
     this.distance = "";
   }
 }
@@ -18,54 +19,38 @@ const cityArray = [];
 
 // 🚨🚨🚨 Get data from database as an array and use loop to make city objects and store them right away
 
-const northVanObj = new City("North Vancouver", [-123.12582, 49.351542]);
-const richmondObj = new City("Richmond", [-123.108487, 49.173966]);
-const surreyObj = new City("Surrey", [-122.841305, 49.15978]);
-const abbotsfordObj = new City("Abbotsford", [-122.323466, 49.065087]);
-const coquitlamObj = new City("Coquitlam", [-122.776836, 49.283485]);
-// const chilliwackObj= new City("Chilliwack", [-121.954941, 49.150314]);
-// const victoriaObj = new City("Victoria", [-123.422233, 48.523145]);
-// const kelownaObj = new City("Kelowna", [-119.455072, 49.869284]);
+for (let i in geo.storageLocationArr) {
+  let stName = geo.storageLocationArr[i][1].name;
+  let stGeoLoc = [geo.storageLocationArr[i][1].geoInfo._long, geo.storageLocationArr[i][1].geoInfo._lat];
+  let stAddress = `${geo.storageLocationArr[i][1].address.street}, ${geo.storageLocationArr[i][1].address.city}, ${geo.storageLocationArr[i][1].address.zipCode}`;
+  let cityObj = new City(stName, stGeoLoc, stAddress);
+  cityArray.push(cityObj);
+}
 
 // 🚨 DUE TO QPS (QUOTA PER SECOND, WHICH IS 5 QUOTAS PER SECOND) OF ROUTING API FOR FREE ACOUNT, I LIMITED THE NUMBER OF LOCATIONS ONLY 5. AFTER ASKING AMANDEEP, WE MAY BE ABLE TO ADD THE LAST THREE CITIES AGAIN　🚨
 
-cityArray.push(northVanObj);
-cityArray.push(richmondObj);
-cityArray.push(surreyObj);
-cityArray.push(abbotsfordObj);
-cityArray.push(coquitlamObj);
-// cityArray.push(chilliwackObj);
-// cityArray.push(victoriaObj);
-// cityArray.push(kelownaObj);
-
+console.log("👇 A list of cities in an array")
 console.log(cityArray);
-
-// References
-// Marker page: https://developer.tomtom.com/maps-sdk-web-js/tutorials/use-cases/how-add-and-customize-location-marker
-// Search bar and map display: https://developer.tomtom.com/maps-sdk-web-js/tutorials/basic/searchbox-integration
-// Calculate distance and eat: https://github.com/joserojas-tomtom/optimize-routes/commit/51a18b433141b4a08c695868b5d8f05a7da362e3
-// Calculate distance and eat: https://developer.tomtom.com/blog/build-different/understanding-how-tomtom-routing-api-provides-accurate-etas/
 
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     (position) => {
-      // GET LOCATION =================================
+      // GET LOCATION ============================================
+      // 🚨 Will change this to user's location 🚨
       let currentLgt = position.coords.longitude;
       let currentLat = position.coords.latitude;
       const currentLoc = [];
       currentLoc.push(currentLgt);
       currentLoc.push(currentLat);
-      console.log(currentLoc);
 
-      // CALCULATE DINTANCE FOR EACH LOCATION =============
+      // CALCULATE DINTANCE FOR EACH LOCATION ==================
 
-      // 🚨 WHY IS NOT THIS WORKING PROPERLY??????? Doesn't fire sort after all the city's ditance calculation 🚨
-
+      //  Array container to contain all the promises for calculating distance
       let promises = [];
 
+      //  Calculata distance in a promise here and return distance.
       for (let i in cityArray) {
         let twoLocs = [currentLoc, cityArray[i].geoLoc];
-        console.log(twoLocs);
 
         promises.push(
           new Promise((resolve, reject) => {
@@ -77,7 +62,7 @@ if (navigator.geolocation) {
               })
               .then((result) => {
                 cityArray[i].distance = result.routes[0].summary.lengthInMeters;
-                console.log("Resolved", i);
+                console.log("Calc distance Resolved", i);
                 resolve();
               });
           })
@@ -86,54 +71,43 @@ if (navigator.geolocation) {
 
       waitforpromises();
 
+      // This function waits until all the calculation promises are done. And once all of them are done, it fires sort function below
       async function waitforpromises() {
         await Promise.all(promises);
-        console.log("All Resolved");
-        tr();
+        console.log("All calc distance Resolved");
+        sortAndShow();
       }
 
-      async function tr() {
-        // SORT LOCATIONS ====================================
+      // SORT AND SHOW IN THE BAR ================================
+      async function sortAndShow() {
+        // SORT
         cityArray.sort(compare);
+        console.log("👇 The array sorted by distance")
         console.log(cityArray);
 
-        // SHOW ON THE SIDE MENU ================================
-        // for (let i in cityArray) {
-        //   let locNum = i+1
-        //   let idName = `"location-${locNum}"`
-        //   document.getElementById(idName).innerHTML = `Location: ${cityArray[i].name}<br>Distance: ${cityArray[i].distance} mtrs`
-        // }
-        document.getElementById("location-1").innerHTML = `Location: ${
-          cityArray[0].name
-        }<br>Distance: ${
-          Math.round(cityArray[0].distance / 10) / 100
-        } kilo mtrs`;
-        document.getElementById("location-2").innerHTML = `Location: ${
-          cityArray[1].name
-        }<br>Distance: ${
-          Math.round(cityArray[1].distance / 10) / 100
-        } kilo mtrs`;
-        document.getElementById("location-3").innerHTML = `Location: ${
-          cityArray[2].name
-        }<br>Distance: ${
-          Math.round(cityArray[2].distance / 10) / 100
-        } kilo mtrs`;
-        document.getElementById("location-4").innerHTML = `Location: ${
-          cityArray[3].name
-        }<br>Distance: ${
-          Math.round(cityArray[3].distance / 10) / 100
-        } kilo mtrs`;
-        document.getElementById("location-5").innerHTML = `Location: ${
-          cityArray[4].name
-        }<br>Distance: ${
-          Math.round(cityArray[4].distance / 10) / 100
-        } kilo mtrs`;
+        // SHOW ON THE SIDE MENU
+        for (let i in cityArray) {
+          let idName = `location-${i}`
+          document.getElementById(idName).value = cityArray[i].name;
+          document.querySelector(`[for="${idName}"]`).innerHTML = `Location: ${cityArray[i].name}<br>Distance: ${cityArray[i].distance} mtrs`
+        }
       }
 
       // FUNCTION FOR SORTING =========================================
       function compare(a, b) {
         return a.distance - b.distance;
       }
+
+      // SELECTED CITY'S VALUE IS STORED HERE=======================
+
+      var selectedCity = "";
+      const submitBtn = document.getElementById("submit");
+      submitBtn.addEventListener("click", function(event) {
+        event.preventDefault();
+        selectedCity = document.querySelector(`input[name="location"]:checked`).value;
+        console.log(selectedCity);
+        alert(`Location: ${selectedCity} was saved. If you want to change your storage location preference, you can choose a defferent location and press submit putton.`);
+      })
 
       // SET MAP =======================================================
       const map = tt.map({
@@ -153,81 +127,17 @@ if (navigator.geolocation) {
         right: [-25, -35],
       };
 
-      //　🚨🚨🚨 Use loop to put makers on each point. Regardless of its name, it's gonna use an array which contains name, location details and everything. So doesn't need to care about which city I'm handling.
-
-      var nvEl = document.createElement("div");
-      nvEl.className = "loc-marker";
-      var northVan = new tt.Marker({ element: nvEl })
-        .setLngLat(northVanObj.geoLoc)
-        .addTo(map);
-      var nvPop = new tt.Popup({ offset: popupOffsets }).setHTML(
-        "<b>North Vancouver</b><br>420 Southborough Dr, West Vancouver, BC V7S 1M2"
-      );
-      northVan.setPopup(nvPop).togglePopup();
-
-      var rchEl = document.createElement("div");
-      rchEl.className = "loc-marker";
-      var richmond = new tt.Marker({ element: rchEl })
-        .setLngLat(richmondObj.geoLoc)
-        .addTo(map);
-      var richPop = new tt.Popup({ offset: popupOffsets }).setHTML(
-        "<b>Richmond</b><br>420 Southborough Dr, West Vancouver, BC V7S 1M2"
-      );
-      richmond.setPopup(richPop).togglePopup();
-
-      var surrEl = document.createElement("div");
-      surrEl.className = "loc-marker";
-      var surrey = new tt.Marker({ element: surrEl })
-        .setLngLat(surreyObj.geoLoc)
-        .addTo(map);
-      var surPop = new tt.Popup({ offset: popupOffsets }).setHTML(
-        "<b>Surrey</b><br>address here"
-      );
-      surrey.setPopup(surPop).togglePopup();
-
-      var abtfEl = document.createElement("div");
-      abtfEl.className = "loc-marker";
-      var abbotsford = new tt.Marker({ element: abtfEl })
-        .setLngLat(abbotsfordObj.geoLoc)
-        .addTo(map);
-      var abtfPop = new tt.Popup({ offset: popupOffsets }).setHTML(
-        "<b>Abbotsford</b><br>address here"
-      );
-      abbotsford.setPopup(abtfPop).togglePopup();
-
-      var coqEl = document.createElement("div");
-      coqEl.className = "loc-marker";
-      var coquitlam = new tt.Marker({ element: coqEl })
-        .setLngLat(coquitlamObj.geoLoc)
-        .addTo(map);
-      var coqPop = new tt.Popup({ offset: popupOffsets }).setHTML(
-        "<b>Coquitlam</b><br>address here"
-      );
-      coquitlam.setPopup(coqPop).togglePopup();
-
-      // var chilEl = document.createElement("div");
-      // chilEl.className = "loc-marker"
-      // var chilliwack = new tt.Marker({ element: chilEl }).setLngLat(chilliwackObj.geoLoc).addTo(map);
-      // var chilPop = new tt.Popup({ offset: popupOffsets }).setHTML(
-      //   "<b>Chilliwack</b><br>address here"
-      // )
-      // chilliwack.setPopup(chilPop).togglePopup()
-
-      // var vicEl = document.createElement("div");
-      // vicEl.className = "loc-marker"
-      // var victoria = new tt.Marker({ element: vicEl }).setLngLat(victoriaObj.geoLoc).addTo(map);
-      // var vicPop = new tt.Popup({ offset: popupOffsets }).setHTML(
-      //   "<b>Victoria</b><br>address here"
-      // )
-      // victoria.setPopup(vicPop).togglePopup()
-
-      // var keloEl = document.createElement("div");
-      // keloEl.className = "loc-marker"
-      // var kelowna = new tt.Marker({ element: keloEl }).setLngLat(kelownaObj.geoLoc).addTo(map);
-      // var keloPop = new tt.Popup({ offset: popupOffsets }).setHTML(
-      //   "<b>Kelowna</b><br>address here"
-      // )
-      // kelowna.setPopup(keloPop).togglePopup()
+      for (let i in cityArray) {
+        var element = document.createElement("div");
+        element.className = "loc-marker";
+        var newMarker = new tt.Marker({ element: element })
+          .setLngLat(cityArray[i].geoLoc)
+          .addTo(map);
+        var newPopUp = new tt.Popup({ offset: popupOffsets }).setHTML(
+          `<b>${cityArray[i].name}</b><br>${cityArray[i].address}`
+        );
+        newMarker.setPopup(newPopUp).togglePopup();
+      }
 
       // MAP ON LOAD WITH ICON ==============================
 
