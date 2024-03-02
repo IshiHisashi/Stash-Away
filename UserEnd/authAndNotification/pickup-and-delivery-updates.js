e2bd40db54fb0ceb2acb0095a2b6d924d0500683
Sacha <150120887+c-est-sa@users.noreply.github.com>
2024-03-02 12:12:10 -0800

    created "common_auth.js" to share the currently logged-in user among all files
    - You can export "getCurrentUid" and call "await getCurrentUid()" in your JS file to get the current user ID. (This is an asynchronous method.)
    - If this works without any problem, we can copy and paste the code in common_auth.js to common.js and delete common_auth.js.

--- UserEnd/authAndNotification/pickup-and-delivery-updates.js
+++ UserEnd/authAndNotification/pickup-and-delivery-updates.js
@@ -1,5 +1,3 @@
-import { onAuthStateChanged, signOut, auth } from "./firebase_auth.js";
-
 import {
   collection,
   doc,
@@ -9,6 +7,13 @@ import {
   where,
   db,
 } from "./firebase_firestore.js";
+
+import {
+  getCurrentUid,
+  getCurrentUserObj,
+  signOut,
+  auth,
+} from "../../common_auth.js";
 
 const createOrderDivElement = (type, textContent = null) => {
   const element = document.createElement(type);
@@ -39,128 +44,123 @@ const showUpdate = (section, orderID, orderDate, itemKey) => {
   });
 
   section.appendChild(containerDiv);
-  // section.appendChild(document.createElement("hr"));
 };
 
+const loginStatusSpan = document.querySelector(".loginStatus");
+
 // get currently logged in user.
-onAuthStateChanged(auth, (user) => {
-  const loginStatusSpan = document.querySelector(".loginStatus");
+const uid = await getCurrentUid();
+if (uid) {
+  loginStatusSpan.textContent = `Logged in: (user ID: ${uid})`;
 
-  if (user) {
-    const uid = user.uid;
-    console.log(user);
-    loginStatusSpan.textContent = `Logged in: (user ID: ${uid})`;
+  // query criteria: all the documents under the user's 'order' collection, except it's "done".
+  (async () => {
+    const q = query(
+      collection(doc(collection(db, "users"), uid), "order"),
+      where("status", "!=", "done")
+    );
+    const objArr = [];
+    const order = await getDocs(q);
+    order.forEach((el) => {
+      const obj = { [el.id]: el.data() };
+      objArr.push(obj);
+    });
+    console.log(objArr);
 
-    // query criteria: all the documents under the user's 'order' collection, except it's "done".
-    (async () => {
-      const q = query(
-        collection(doc(collection(db, "users"), uid), "order"),
-        where("status", "!=", "done")
-      );
-      const objArr = [];
-      const order = await getDocs(q);
-      order.forEach((el) => {
-        const obj = { [el.id]: el.data() };
-        objArr.push(obj);
-      });
-      console.log(objArr);
+    objArr.forEach((orderObj) => {
+      const orderID = Object.keys(orderObj)[0];
+      const status = orderObj[orderID].status;
 
-      objArr.forEach((orderObj) => {
-        const orderID = Object.keys(orderObj)[0];
-        const status = orderObj[orderID].status;
+      // don't show 'done' status order
+      if (status !== "done") {
+        const orderDate = orderObj[orderID].orderDate;
+        const address = `${orderObj[orderID].address.detail}, ${orderObj[orderID].address.city}, ${orderObj[orderID].address.province} ${orderObj[orderID].address.zipCode}`;
+        const itemKey = orderObj[orderID].itemKey;
 
-        // don't show 'done' status order
-        if (status !== "done") {
-          const orderDate = orderObj[orderID].orderDate;
-          const address = `${orderObj[orderID].address.detail}, ${orderObj[orderID].address.city}, ${orderObj[orderID].address.province} ${orderObj[orderID].address.zipCode}`;
-          const itemKey = orderObj[orderID].itemKey;
+        const orderType = orderObj[orderID].orderType;
+        const section = document.querySelector(
+          orderType === "add" ? ".adding" : ".retrieval"
+        );
 
-          const orderType = orderObj[orderID].orderType;
-          const section = document.querySelector(
-            orderType === "add" ? ".adding" : ".retrieval"
+        showUpdate(section, orderID, orderDate, itemKey);
+
+        // If the data has ETA in the first place, show it at the same time.
+        if (orderObj[orderID].ETA) {
+          const rawTimestamp = new Date(
+            orderObj[orderID].departTimestamp.seconds * 1000 +
+              orderObj[orderID].departTimestamp.nanoseconds / 1000000
           );
+          const formattedTimestamp = rawTimestamp.toLocaleString("en-CA", {
+            timeZone: "America/Vancouver",
+            weekday: "short",
+            year: "numeric",
+            month: "short",
+            day: "numeric",
+            hour: "numeric",
+            minute: "2-digit",
+            timeZoneName: "shortGeneric",
+          });
+          const h4 = createOrderDivElement("h4", "DRIVER DEPARTED");
+          const p = createOrderDivElement("p", formattedTimestamp);
+          document.querySelector(`#${orderID}`).appendChild(h4);
+          document.querySelector(`#${orderID}`).appendChild(p);
 
-          showUpdate(section, orderID, orderDate, itemKey);
+          const h4ETA = createOrderDivElement("h4", "ETA");
+          const pETA = createOrderDivElement("p", orderObj[orderID].ETA);
+          document.querySelector(`#${orderID}`).appendChild(h4ETA);
+          document.querySelector(`#${orderID}`).appendChild(pETA);
+        }
+      }
+    });
+  })();
 
-          // If the data has ETA in the first place, show it at the same time.
-          if (orderObj[orderID].ETA) {
-            const rawTimestamp = new Date(
-              orderObj[orderID].departTimestamp.seconds * 1000 +
-                orderObj[orderID].departTimestamp.nanoseconds / 1000000
-            );
-            const formattedTimestamp = rawTimestamp.toLocaleString("en-CA", {
-              timeZone: "America/Vancouver",
-              weekday: "short",
-              year: "numeric",
-              month: "short",
-              day: "numeric",
-              hour: "numeric",
-              minute: "2-digit",
-              timeZoneName: "shortGeneric",
-            });
-            const h4 = createOrderDivElement("h4", "DRIVER DEPARTED");
-            const p = createOrderDivElement("p", formattedTimestamp);
-            document.querySelector(`#${orderID}`).appendChild(h4);
-            document.querySelector(`#${orderID}`).appendChild(p);
+  (async () => {
+    // query criteria: all the documents under the user's 'order' collection
+    const q = query(collection(doc(collection(db, "users"), uid), "order"));
 
-            const h4ETA = createOrderDivElement("h4", "ETA");
-            const pETA = createOrderDivElement("p", orderObj[orderID].ETA);
-            document.querySelector(`#${orderID}`).appendChild(h4ETA);
-            document.querySelector(`#${orderID}`).appendChild(pETA);
-          }
+    // add change listener to the queried place
+    const unsubscribe = await onSnapshot(q, (querySnapshot) => {
+      // get an array of the documents changes since the last snapshot.
+      querySnapshot.docChanges().forEach((changedDoc) => {
+        if (
+          changedDoc.type === "modified" &&
+          changedDoc.doc.data().status === "on going"
+        ) {
+          const rawTimestamp = new Date(
+            changedDoc.doc.data().departTimestamp.seconds * 1000 +
+              changedDoc.doc.data().departTimestamp.nanoseconds / 1000000
+          );
+          const formattedTimestamp = rawTimestamp.toLocaleString("en-CA", {
+            timeZone: "America/Vancouver",
+            weekday: "short",
+            year: "numeric",
+            month: "short",
+            day: "numeric",
+            hour: "numeric",
+            minute: "2-digit",
+            timeZoneName: "shortGeneric",
+          });
+
+          const h4 = createOrderDivElement("h4", "DRIVER DEPARTED");
+          const p = createOrderDivElement("p", formattedTimestamp);
+          document.querySelector(`#${changedDoc.doc.id}`).appendChild(h4);
+          document.querySelector(`#${changedDoc.doc.id}`).appendChild(p);
+
+          const formattedETA = changedDoc.doc.data().ETA;
+
+          const h4ETA = createOrderDivElement("h4", "ETA");
+          const pETA = createOrderDivElement("p", formattedETA);
+          document.querySelector(`#${changedDoc.doc.id}`).appendChild(h4ETA);
+          document.querySelector(`#${changedDoc.doc.id}`).appendChild(pETA);
+
+          // notification
+          new Notification("StashAway", {
+            body: `The driver is on the way! Order ID: ${changedDoc.doc.id}, ETA: ${formattedETA}`,
+          });
         }
       });
-    })();
-
-    (async () => {
-      // query criteria: all the documents under the user's 'order' collection
-      const q = query(collection(doc(collection(db, "users"), uid), "order"));
-
-      // add change listener to the queried place
-      const unsubscribe = await onSnapshot(q, (querySnapshot) => {
-        // get an array of the documents changes since the last snapshot.
-        querySnapshot.docChanges().forEach((changedDoc) => {
-          if (
-            changedDoc.type === "modified" &&
-            changedDoc.doc.data().status === "on going"
-          ) {
-            const rawTimestamp = new Date(
-              changedDoc.doc.data().departTimestamp.seconds * 1000 +
-                changedDoc.doc.data().departTimestamp.nanoseconds / 1000000
-            );
-            const formattedTimestamp = rawTimestamp.toLocaleString("en-CA", {
-              timeZone: "America/Vancouver",
-              weekday: "short",
-              year: "numeric",
-              month: "short",
-              day: "numeric",
-              hour: "numeric",
-              minute: "2-digit",
-              timeZoneName: "shortGeneric",
-            });
-
-            const h4 = createOrderDivElement("h4", "DRIVER DEPARTED");
-            const p = createOrderDivElement("p", formattedTimestamp);
-            document.querySelector(`#${changedDoc.doc.id}`).appendChild(h4);
-            document.querySelector(`#${changedDoc.doc.id}`).appendChild(p);
-
-            const formattedETA = changedDoc.doc.data().ETA;
-
-            const h4ETA = createOrderDivElement("h4", "ETA");
-            const pETA = createOrderDivElement("p", formattedETA);
-            document.querySelector(`#${changedDoc.doc.id}`).appendChild(h4ETA);
-            document.querySelector(`#${changedDoc.doc.id}`).appendChild(pETA);
-
-            // notification
-            new Notification("StashAway", {
-              body: `The driver is on the way! Order ID: ${changedDoc.doc.id}, ETA: ${formattedETA}`,
-            });
-          }
-        });
-      });
-    })();
-  } else {
-    console.log("no user logged in");
-    loginStatusSpan.textContent = `Logged out`;
-  }
-});
+    });
+  })();
+} else {
+  loginStatusSpan.textContent = `Logged out`;
+}
