e3bc699b894827ee95c18bfe424832a585a5677f
Akiodesukedo <abtai0227aki@gmail.com>
2024-03-07 15:19:57 -0800

    Profile page date update
    Now the profile page can update data on firebase

--- UserEnd/profile/profile.js
+++ UserEnd/profile/profile.js
@@ -2,6 +2,19 @@ import * as common from "../../common.js";
 
 // GET CURRENTLY LOGGED IN ACCOUNT INFO
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA0Px8PkiCzyTrDcFCWh-mbER-YcWd9d-E",
+  authDomain: "fir-jan24.firebaseapp.com",
+  projectId: "fir-jan24",
+  storageBucket: "fir-jan24.appspot.com",
+  messagingSenderId: "831417179844",
+  appId: "1:831417179844:web:c3eb03b7fc9c6ef7b03391",
+  measurementId: "G-DSYKEF99M1",
+};
+const app = initializeApp(firebaseConfig);
+const db = common.getFirestore(app);
 const uid = await common.getCurrentUid();
 console.log("User id downloaded")
 console.log(uid);
@@ -64,7 +77,7 @@ function getProfileInfo() {
         document.getElementById("plan-price").style.textDecoration = "line-through";
     }
 
-    let discountedPrice = originalPrice * planDetailsByDuraton.discount;
+    let discountedPrice = Math.round(originalPrice * planDetailsByDuraton.discount * 10 ) / 10;
     document.getElementById("discounted").innerHTML = `$${discountedPrice}/month`;
 
     document.getElementById("free-trip").innerHTML = `${planDetailsByDuraton.numTrip} Free Trips`
@@ -86,58 +99,35 @@ function getProfileInfo() {
         }
     
         createInnerHtml() {
+            let label = document.createElement("label");
+            label.setAttribute("for", `card-${this.number}`);
+            let image = document.createElement("img");
+            // image.setAttribute("src", `${this.cardBrandUrl}`);
+            image.setAttribute("class", "card-brand-img");
+            let cardNumPara = document.createElement("p");
+            cardNumPara.setAttribute("class", "card-num");
+            let node = document.createTextNode(`${this.cardnum}`);
+            cardNumPara.appendChild(node);
+            let expiration = document.createElement("p");
+            expiration.setAttribute("class", "exp-date");
+            let node2 = document.createTextNode(this.expDate);
+            expiration.appendChild(node2);
+            let cardRadioBtn = document.createElement("input");
+            cardRadioBtn.setAttribute("type", "radio");
+            cardRadioBtn.setAttribute("id", `card-${this.number}`);
+            cardRadioBtn.setAttribute("name", "card-number");
+            cardRadioBtn.setAttribute("value", `${this.cardnum}`);
             if (this.defaultBoolean == true) {
-                let label = document.createElement("label");
-                label.setAttribute("for", `card-${this.number}`);
-                let image = document.createElement("img");
-                // image.setAttribute("src", `${this.cardBrandUrl}`);
-                image.setAttribute("class", "card-brand-img");
-                let cardNumPara = document.createElement("p");
-                cardNumPara.setAttribute("class", "card-num");
-                let node = document.createTextNode(`${this.cardnum}`);
-                cardNumPara.appendChild(node);
-                // let expiration = document.createElement("p");
-                // expiration.setAttribute("class", "exp-date");
-                // var date = new Date(this.expDate * 1000)
-                // let node2 = document.createTextNode(`${date}`);
-                // expiration.appendChild(node2);
-                let cardRadioBtn = document.createElement("input");
-                cardRadioBtn.setAttribute("type", "radio");
-                cardRadioBtn.setAttribute("id", `card-${this.number}`);
-                cardRadioBtn.setAttribute("name", "card-number");
-                cardRadioBtn.setAttribute("value", `${this.cardnum}`);
                 cardRadioBtn.setAttribute("checked", "");
-                label.appendChild(image);
-                label.appendChild(cardNumPara);
-                // label.appendChild(expiration);
-                let eachCard = document.createElement("div");
-                eachCard.setAttribute("class", "each-card")
-                eachCard.appendChild(label);
-                eachCard.appendChild(cardRadioBtn);
-                fieldSetArea.appendChild(eachCard);
-            } else {
-                let label = document.createElement("label");
-                label.setAttribute("for", `card-${this.number}`);
-                let image = document.createElement("img");
-                // image.setAttribute("src", `${this.cardBrandUrl}`);
-                image.setAttribute("class", "card-brand-img");
-                let cardNumPara = document.createElement("p");
-                cardNumPara.setAttribute("class", "card-num");
-                let node = document.createTextNode(`${this.cardnum}`);
-                cardNumPara.appendChild(node);
-                let cardRadioBtn = document.createElement("input");
-                cardRadioBtn.setAttribute("type", "radio");
-                cardRadioBtn.setAttribute("id", `card-${this.number}`);
-                cardRadioBtn.setAttribute("name", "card-number");
-                cardRadioBtn.setAttribute("value", `${this.cardnum}`);
-                label.appendChild(image);
-                label.appendChild(cardNumPara);
-                let eachCard = document.createElement("div");
-                eachCard.setAttribute("class", "each-card")
-                eachCard.appendChild(label);
-                eachCard.appendChild(cardRadioBtn);
-                fieldSetArea.appendChild(eachCard);
             }
+            label.appendChild(image);
+            label.appendChild(cardNumPara);
+            label.appendChild(expiration);
+            let eachCard = document.createElement("div");
+            eachCard.setAttribute("class", "each-card")
+            eachCard.appendChild(label);
+            eachCard.appendChild(cardRadioBtn);
+            fieldSetArea.appendChild(eachCard);
             
         }
     }
@@ -166,3 +156,68 @@ function firstDigit(num) {
     const divisor = 10 ** (len - 1);
     return Math.trunc(num / divisor);
 }
+
+// SAVE CHANGE ON PROFILE SECTION =================
+
+const saveProfileBtn = document.getElementById("save-profile");
+saveProfileBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    const inputfields = document.querySelectorAll('input[name="profile-input"]');
+    let firstName = inputfields[0].value;
+    let lastName = inputfields[1].value;
+    let emailAddress = inputfields[2].value;
+    let phoneNum = inputfields[3].value;
+    let roomNumEtc = inputfields[4].value;
+    let addressDetail = inputfields[5].value;
+    let city = inputfields[6].value;
+    let province = inputfields[7].value;
+    let zipCode = inputfields[8].value;
+
+    updateProfileInfo(firstName, lastName, emailAddress, phoneNum, roomNumEtc, addressDetail, city, province, zipCode)
+})
+
+const updateProfileInfo = async function (fName, lName, email, phone, etc, detail, city, province, zip) {
+    await common.updateDoc(common.doc(db, "users", `${uid}`), {
+        "address.cit": city,
+        "address.detail": detail,
+        "address.roomNumEtc": etc,
+        "address.province": province,
+        "address.zipCode": zip,
+        "contact.email": email,
+        "contact.phone": phone,
+        "userName.firstName": fName,
+        "userName.lastName": lName,
+    });
+
+    console.log(`${fName}, ${lName}, ${email}, ${phone}, ${etc}, ${detail}, ${city}, ${province}, ${zip}`);
+};
+
+// RADIO BUTTON ONCHANGED EVENT ============================
+
+const radioBtns = document.querySelectorAll('input[name="card-number"]');
+console.log(radioBtns);
+for (let i = 0; i <= radioBtns.length - 1; i++) {
+    radioBtns[i].addEventListener("click", (e) => {
+        for (let j = 0; j <= radioBtns.length - 1; j++) {
+            if (j == i) {
+                updateDefaultTrue(j);
+            } else {
+                updateDefaultFalse(j);
+            }
+        }
+        alert(`You chose a card with number: ${radioBtns[i].value}`);
+    })
+}
+
+const updateDefaultTrue = async function (arrayNum) {
+    await common.updateDoc(common.doc(db, "users", `${uid}`, `payment_method`, `[${arrayNum}]`), {
+        
+        "default": true,
+    });
+};
+
+const updateDefaultFalse = async function (arrayNum) {
+    await common.updateDoc(common.doc(db, "users", `${uid}`, `payment_method`, `[${arrayNum}]`), {  
+        // "default": false,
+    });
+};
