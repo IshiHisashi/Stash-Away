294d7163b72d34c9a54ea4c073079ae5aee24ec8
Sacha <150120887+c-est-sa@users.noreply.github.com>
2024-03-20 14:28:14 -0700

    styled header (not complete)

--- UserEnd/homepage/header/header.js
+++ UserEnd/homepage/header/header.js
@@ -4,133 +4,133 @@ let user = null;
 const basePath = "../";
 
 async function initHeader() {
-    try {
-        user = await common.getCurrentUserObj();
-        updateUIBasedOnUser(user);
-    } catch (error) {
-        console.error('An error occurred:', error);
-    }
+  try {
+    user = await common.getCurrentUserObj();
+    updateUIBasedOnUser(user);
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
 
-    const btntoggleMenu = document.getElementById('btntoggleMenu');
-if (btntoggleMenu) {
+  const btntoggleMenu = document.getElementById("btntoggleMenu");
+  if (btntoggleMenu) {
     btntoggleMenu.addEventListener("click", async (e) => {
-        e.preventDefault();
-        const nav = document.querySelector('.main-menu');
-        nav.classList.toggle('active');
+      e.preventDefault();
+      const nav = document.querySelector(".main-menu");
+      nav.classList.toggle("active");
     });
-}
+  }
 
-const btnGeoLocation = document.getElementById('btnGeoLocation');
-if (btnGeoLocation) {
+  const btnGeoLocation = document.getElementById("btnGeoLocation");
+  if (btnGeoLocation) {
     btnGeoLocation.addEventListener("click", async (e) => {
-        e.preventDefault();
-        window.location.href = `${basePath}home-page-map/index.html`;
-
+      e.preventDefault();
+      window.location.href = `${basePath}home-page-map/index.html`;
     });
-}
+  }
 
-const btnPricing = document.getElementById('btnPricing');
-if (btnPricing) {
-    btnPricing.addEventListener('click', function () {
-        window.location.href = `${basePath}dummy_payment/dummy_payment.html`;
+  const btnPricing = document.getElementById("btnPricing");
+  if (btnPricing) {
+    btnPricing.addEventListener("click", function () {
+      window.location.href = `${basePath}dummy_payment/dummy_payment.html`;
     });
-}
+  }
 
-const btnHelpCenter = document.getElementById('btnHelpCenter');
-if (btnHelpCenter) {
-    btnHelpCenter.addEventListener('click', function () {
-        alert('Help Center - Not yet developed!');
+  const btnHelpCenter = document.getElementById("btnHelpCenter");
+  if (btnHelpCenter) {
+    btnHelpCenter.addEventListener("click", function () {
+      alert("Help Center - Not yet developed!");
     });
-}
+  }
 
-// const buttonMyAccount = document.querySelector('btnMyAccount');
-// if (buttonMyAccount) {
-//     buttonMyAccount.addEventListener("click", async (e) => {
-//         debugger
-//         e.preventDefault();
-//         if (window.innerWidth > 600) {
-//             var subMenu = e.target.nextElementSibling;
-//             subMenu.style.display = subMenu.style.display === 'block' ? 'none' : 'block';
-//         }
-//     });
-// }
+  // const buttonMyAccount = document.querySelector('btnMyAccount');
+  // if (buttonMyAccount) {
+  //     buttonMyAccount.addEventListener("click", async (e) => {
+  //         debugger
+  //         e.preventDefault();
+  //         if (window.innerWidth > 600) {
+  //             var subMenu = e.target.nextElementSibling;
+  //             subMenu.style.display = subMenu.style.display === 'block' ? 'none' : 'block';
+  //         }
+  //     });
+  // }
 
-const btnOrderUpdate = document.getElementById('btnOrderUpdate');
-if (btnOrderUpdate) {
-    btnOrderUpdate.addEventListener('click', function () {
-        window.location.href = `${basePath}order-confirmation/order-confirmation.html`;
+  const btnOrderUpdate = document.getElementById("btnOrderUpdate");
+  if (btnOrderUpdate) {
+    btnOrderUpdate.addEventListener("click", function () {
+      window.location.href = `${basePath}order-confirmation/order-confirmation.html`;
     });
-}
+  }
 
-const btnViewStorage = document.getElementById('btnViewStorage');
-if (btnViewStorage) {
-    btnViewStorage.addEventListener('click', function () {
-        window.location.href = `${basePath}storage-mgmt/storageMgmt.html`;
+  const btnViewStorage = document.getElementById("btnViewStorage");
+  if (btnViewStorage) {
+    btnViewStorage.addEventListener("click", function () {
+      window.location.href = `${basePath}storage-mgmt/storageMgmt.html`;
     });
-}
+  }
 
-const btnProfile = document.getElementById('btnProfile');
-if (btnProfile) {
-    btnProfile.addEventListener('click', function () {
-        window.location.href = `${basePath}profile/index.html`;
+  const btnProfile = document.getElementById("btnProfile");
+  if (btnProfile) {
+    btnProfile.addEventListener("click", function () {
+      window.location.href = `${basePath}profile/index.html`;
     });
-}
+  }
 
-const btnLogout = document.getElementById('btnLogout');
-if (btnLogout) {
-    btnLogout.addEventListener('click', function () {
-        common
-            .signOut(common.auth)
-            .then(() => {
-                console.log("signed out");
-                window.location.href = "main.html";
-            })
-            .catch((error) => {
-                console.log(error);
-            });
+  const btnLogout = document.getElementById("btnLogout");
+  if (btnLogout) {
+    btnLogout.addEventListener("click", function () {
+      common
+        .signOut(common.auth)
+        .then(() => {
+          console.log("signed out");
+          window.location.href = "main.html";
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     });
-}
+  }
 
-const btnLogin = document.getElementById('btnLogin');
-if (btnLogin) {
-    btnLogin.addEventListener('click', function () {
-        window.location.href = `${basePath}authentication/login.html`;
+  const btnLogin = document.getElementById("btnLogin");
+  if (btnLogin) {
+    btnLogin.addEventListener("click", function () {
+      window.location.href = `${basePath}authentication/login.html`;
     });
-}
+  }
 
-const btnBookNow = document.getElementById('btnBookNow');
-if (btnBookNow) {
+  const btnBookNow = document.getElementById("btnBookNow");
+  if (btnBookNow) {
     btnBookNow.addEventListener("click", async (e) => {
-        e.preventDefault();
-        if (isAuthenticated()) {
-            window.location.href = `${basePath}booking/booking.html`;
-        } else {
-            window.location.href = `${basePath}authentication/login.html?returnUrl=${encodeURIComponent(`${basePath}booking/booking.html`)}`;
-        }
+      e.preventDefault();
+      if (isAuthenticated()) {
+        window.location.href = `${basePath}booking/booking.html`;
+      } else {
+        window.location.href = `${basePath}authentication/login.html?returnUrl=${encodeURIComponent(
+          `${basePath}booking/booking.html`
+        )}`;
+      }
     });
-}
+  }
 
-    // Since this logic might depend on the header being fully loaded,
-    // make sure you call this function after the header HTML has been inserted.
-    window.onclick = function(event) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        for (var i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    };
+  // Since this logic might depend on the header being fully loaded,
+  // make sure you call this function after the header HTML has been inserted.
+  window.onclick = function (event) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (var i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  };
 }
-
 
 function toggleDropdown(event) {
-    document.getElementById("myDropdown").classList.toggle("show");
-    event.stopPropagation();
+  document.getElementById("myDropdown").classList.toggle("show");
+  event.stopPropagation();
 }
 
 const isAuthenticated = () => {
-    return !!user;
+  return !!user;
 };
 
 // const bookNowHero = document.getElementById('bookNowHero');
@@ -141,32 +141,31 @@ const isAuthenticated = () => {
 //     });
 // }
 
-
 function updateUIBasedOnUser(user) {
-    const buttonMyAccount = document.getElementById('btnMyAccount');
-    const buttonOrderUpdate = document.getElementById('btnOrderUpdate');
-    const buttonViewStorage = document.getElementById('btnViewStorage');
-    const buttonProfile = document.getElementById('btnProfile');
-    const buttonLogout = document.getElementById('btnLogout');
-    const buttonLogin = document.getElementById('btnLogin');
-    if (user) {
-        console.log(user.uid);
-        buttonMyAccount.style.display = 'block';
-        buttonOrderUpdate.style.display = 'block';
-        buttonViewStorage.style.display = 'block';
-        buttonProfile.style.display = 'block';
-        buttonLogout.style.display = 'block';
-        buttonLogin.style.display = 'none';
-    } else {
-        console.log('No user is logged in.');
-        buttonMyAccount.style.display = 'none';
-        buttonMyAccount.style.display = 'none';
-        buttonOrderUpdate.style.display = 'none';
-        buttonViewStorage.style.display = 'none';
-        buttonProfile.style.display = 'none';
-        buttonLogout.style.display = 'none';
-        buttonLogin.style.display = 'block';
-    }
+  const buttonMyAccount = document.getElementById("btnMyAccount");
+  const buttonOrderUpdate = document.getElementById("btnOrderUpdate");
+  const buttonViewStorage = document.getElementById("btnViewStorage");
+  const buttonProfile = document.getElementById("btnProfile");
+  const buttonLogout = document.getElementById("btnLogout");
+  const buttonLogin = document.getElementById("btnLogin");
+  if (user) {
+    console.log(user.uid);
+    buttonMyAccount.style.display = "flex";
+    buttonOrderUpdate.style.display = "block";
+    buttonViewStorage.style.display = "block";
+    buttonProfile.style.display = "block";
+    buttonLogout.style.display = "block";
+    buttonLogin.style.display = "none";
+  } else {
+    console.log("No user is logged in.");
+    buttonMyAccount.style.display = "none";
+    buttonMyAccount.style.display = "none";
+    buttonOrderUpdate.style.display = "none";
+    buttonViewStorage.style.display = "none";
+    buttonProfile.style.display = "none";
+    buttonLogout.style.display = "none";
+    buttonLogin.style.display = "block";
+  }
 }
 
 export { initHeader };
